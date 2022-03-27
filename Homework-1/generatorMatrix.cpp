a59ae1fb6d2e8367b829fd87b906d3a57c7ccd49
DavidePare <davipar@tiscali.it>
2022-03-27 23:16:09 +0200

    Assignment + generator
    I have just a bug to data : with 1 row or 1 column.
    Not only that but also i have the warning for value like 2 into the matrix.

---
+++ Homework-1/generatorMatrix.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+#define sz(x) (int)x.size()
+#define pb push_back
+
+using namespace std;
+
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int numberfile;
+	cin>>numberfile;
+	while(numberfile--){	
+		int x=rand() % 100 + 2;
+		int y=rand() % 100 + 2;
+		cout<<x<<"  "<<y<<endl;
+		fstream my_file;
+		string filename="fileData";
+		filename+=to_string(numberfile);
+		filename+=".dzn";  
+		cout<<filename;
+		my_file.open(filename, ios::out);
+		if (!my_file) {
+			cout << "File not created!";
+		}
+		else {
+			my_file<< "R=";
+			my_file<< x;
+			my_file<< ";\nC=";
+			my_file<< y;
+			my_file<< ";\nbomb=[|";
+
+			cout << "File created successfully!";
+			for(int i=0;i<x;i++){
+				for(int j=0;j<y;j++){
+					int valore=rand() % 1000 ;
+					cout<<valore<<"\n";
+					if(valore< 950){
+						my_file << "0";
+					}else my_file << "M";
+					my_file << ",";
+				}
+				if(i!=x-1) my_file<< "|\n";
+			}
+			my_file<<"|];";
+			my_file.close();
+		}
+	}
+
+
+}
